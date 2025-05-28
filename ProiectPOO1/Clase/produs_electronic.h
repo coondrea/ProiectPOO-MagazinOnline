215b7e472148d953d5ec9bcb214962f035bf9921
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 01:04:26 +0300

    Update produs_electronic.h

--- ProiectPOO1/Clase/produs_electronic.h
+++ ProiectPOO1/Clase/produs_electronic.h
@@ -5,25 +5,39 @@
 
 namespace Online_Store {
 
+// Clasa care reprezinta un produs electronic derivat din clasa de baza Produs
 class ProdusElectronic : public Produs {
 private:
-    int m_garantie_luni;
+    int m_garantie_luni; // numarul de luni garantie
 
+    // Metoda privata pentru validarea valorii garantiei (intre 0 si 60 luni)
     void _ValidareGarantie(int luni);
 
 public:
+    // Constructor implicit
     ProdusElectronic();
+
+    // Constructor cu parametri pentru initializarea completa a produsului
     ProdusElectronic(int id, const std::string& nume, const std::string& categorie,
                      double pret, int stoc, int garantie);
+
+    // Destructor
     ~ProdusElectronic();
 
+    // Returneaza numarul de luni garantie
     int GetGarantie() const;
+
+    // Seteaza valoarea garantiei dupa validare
     void SetGarantie(int luni);
 
+    // Afiseaza detaliile complete ale produsului (suprascriere metoda virtuala)
     void AfiseazaDetalii() const override;
+
+    // Returneaza o descriere detaliata a produsului (suprascriere metoda virtuala)
     std::string GetDescriereDetaliata() const override;
 };
 
 } // namespace Online_Store
 
 #endif
+
