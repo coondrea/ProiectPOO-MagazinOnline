e24405f0eb8f709a3b7cf1fc0ff7405855fe69bb
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 00:53:10 +0300

    Update client.h

--- ProiectPOO1/Clase/client.h
+++ ProiectPOO1/Clase/client.h
@@ -6,17 +6,29 @@
 
 namespace Online_Store {
 
+/*
+ * Clasa Client mosteneste clasa Utilizator si adauga functionalitati specifice unui client:
+ * adresa de livrare si o lista de comenzi (identificate prin ID-uri).
+ */
 class Client : public Utilizator {
 private:
-    std::string m_adresa_livrare;
-    std::vector<int> m_id_comenzi; // ID-uri comenzi (simplificat deocamdată)
+    std::string m_adresa_livrare;        // Adresa de livrare asociata clientului
+    std::vector<int> m_id_comenzi;       // Lista de ID-uri ale comenzilor plasate
 
 public:
+    // Constructor implicit
     Client();
+
+    // Constructor cu parametri pentru initializarea tuturor atributelor clientului
     Client(int id, const std::string& nume_utilizator, const std::string& email, const std::string& adresa);
 
+    // Adauga ID-ul unei comenzi la lista comenzilor clientului
     void AdaugaComanda(int id_comanda);
+
+    // Returneaza tipul utilizatorului - "Client"
     std::string GetTipUtilizator() const override;
+
+    // Afiseaza toate informatiile despre client
     void AfiseazaProfil() const override;
 };
 
