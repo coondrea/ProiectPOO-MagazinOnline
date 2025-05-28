7883f4c1455ae72bf6207c95e2c5a2a600ec9589
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 01:04:52 +0300

    Update utilizator.cpp

--- ProiectPOO1/Clase/utilizator.cpp
+++ ProiectPOO1/Clase/utilizator.cpp
@@ -2,28 +2,37 @@
 
 namespace Online_Store {
 
-Utilizator::Utilizator() : m_id(0), m_nume_utilizator("Anonim"), m_email("necunoscut@exemplu.com") {}
+// Constructor implicit care initializeaza valorile cu default
+Utilizator::Utilizator() 
+    : m_id(0), m_nume_utilizator("Anonim"), m_email("necunoscut@exemplu.com") {}
 
+// Constructor cu parametri pentru initializarea utilizatorului
 Utilizator::Utilizator(int id, const std::string& nume_utilizator, const std::string& email)
     : m_id(id), m_nume_utilizator(nume_utilizator), m_email(email) {}
 
+// Destructor virtual
 Utilizator::~Utilizator() {}
 
+// Returneaza ID-ul utilizatorului
 int Utilizator::GetId() const {
     return m_id;
 }
 
+// Returneaza numele de utilizator
 std::string Utilizator::GetNumeUtilizator() const {
     return m_nume_utilizator;
 }
 
+// Returneaza adresa de email a utilizatorului
 std::string Utilizator::GetEmail() const {
     return m_email;
 }
 
+// Seteaza o noua adresa de email pentru utilizator
 void Utilizator::SetEmail(const std::string& email) {
     m_email = email;
 }
 
 } // namespace Online_Store
+
 
