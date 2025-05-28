53fdb394eda32848f3181c818dd40b78e746edc4
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 00:50:39 +0300

    Update administrator.cpp

--- ProiectPOO1/Clase/administrator.cpp
+++ ProiectPOO1/Clase/administrator.cpp
@@ -3,17 +3,23 @@
 
 namespace Online_Store {
 
+/* Constructor implicit pentru Administrator.
+   Initializeaza cu valori default mostenite din Utilizator si departamentul ca "General". */
 Administrator::Administrator()
     : Utilizator(), m_departament("General") {}
 
+/* Constructor cu parametri pentru Administrator.
+   Primeste id, nume de utilizator, email si departamentul asociat. */
 Administrator::Administrator(int id, const std::string& nume_utilizator,
                              const std::string& email, const std::string& departament)
     : Utilizator(id, nume_utilizator, email), m_departament(departament) {}
 
+/* Returneaza tipul utilizatorului sub forma de string. Este utilizat pentru identificarea rolului. */
 std::string Administrator::GetTipUtilizator() const {
     return "Administrator";
 }
 
+/* Afiseaza informatiile detaliate ale unui administrator. */
 void Administrator::AfiseazaProfil() const {
     std::cout << "Profil Administrator:\n";
     std::cout << "ID: " << m_id << "\nNume: " << m_nume_utilizator
@@ -21,10 +27,14 @@ void Administrator::AfiseazaProfil() const {
               << "\nDepartament: " << m_departament << "\n";
 }
 
+/* Functie placeholder pentru adaugarea unui produs.
+   Poate fi extinsa pentru a interactiona cu catalogul magazinului. */
 void Administrator::AdaugaProdus() {
     std::cout << "Stub: adaugare produs\n";
 }
 
+/* Functie placeholder pentru stergerea unui produs.
+   La fel ca AdaugaProdus, poate fi extinsa pentru a opera asupra produselor existente. */
 void Administrator::StergeProdus() {
     std::cout << "Stub: stergere produs\n";
 }
