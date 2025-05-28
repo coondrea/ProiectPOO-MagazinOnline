963e836637b4c1ad8423dd12734ec1a76e1cab27
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 00:56:52 +0300

    Update cos_cumparaturi.h

--- ProiectPOO1/Clase/cos_cumparaturi.h
+++ ProiectPOO1/Clase/cos_cumparaturi.h
@@ -7,22 +7,42 @@
 
 namespace Online_Store {
 
+/*
+ * Clasa CosCumparaturi gestioneaza lista de produse adaugate de un client.
+ * Aceasta permite adaugarea, stergerea, afisarea si golirea produselor,
+ * precum si obtinerea totalului si a numarului de produse.
+ */
 class CosCumparaturi {
 private:
-    std::vector<std::shared_ptr<Produs>> m_produse;
-    double m_total;
+    std::vector<std::shared_ptr<Produs>> m_produse;  // Produsele adaugate in cos
+    double m_total;                                  // Totalul valoric al produselor din cos
 
+    // Functie privata care recalculeaza totalul cosului dupa fiecare modificare
     void _ActualizeazaTotal();
 
 public:
+    // Constructor implicit
     CosCumparaturi();
 
+    // Adauga un produs in cos
     void AdaugaProdus(const std::shared_ptr<Produs>& produs);
+
+    // Sterge un produs din cos pe baza ID-ului
     void StergeProdus(int id_produs);
+
+    // Elimina toate produsele din cos
     void GolesteCosul();
+
+    // Afiseaza continutul cosului si totalul
     void AfiseazaCos() const;
+
+    // Returneaza totalul curent al cosului
     double GetTotal() const;
+
+    // Returneaza numarul de produse din cos
     int GetNrProduse() const;
+
+    // Verifica daca cosul este gol
     bool EsteGol() const;
 };
 
