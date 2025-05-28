ed81b3e92a3665c32536d49f377ea54b3848ec23
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 01:03:47 +0300

    Update produs_electronic.cpp

--- ProiectPOO1/Clase/produs_electronic.cpp
+++ ProiectPOO1/Clase/produs_electronic.cpp
@@ -4,8 +4,10 @@
 
 namespace Online_Store {
 
+// Constructor implicit - initializeaza cu garantie 0
 ProdusElectronic::ProdusElectronic() : Produs(), m_garantie_luni(0) {}
 
+// Constructor cu parametri - initializeaza toate atributele, inclusiv validare garantie
 ProdusElectronic::ProdusElectronic(int id, const std::string& nume, const std::string& categorie,
                                    double pret, int stoc, int garantie)
     : Produs(id, nume, categorie, pret, stoc) {
@@ -13,22 +15,27 @@ ProdusElectronic::ProdusElectronic(int id, const std::string& nume, const std::s
     m_garantie_luni = garantie;
 }
 
+// Destructor
 ProdusElectronic::~ProdusElectronic() {}
 
+// Metoda privata care verifica daca valoarea garantiei este valida
 void ProdusElectronic::_ValidareGarantie(int luni) {
     if (luni < 0 || luni > 60)
         throw std::invalid_argument("Garantie invalida (0-60 luni)");
 }
 
+// Getter pentru numarul de luni garantie
 int ProdusElectronic::GetGarantie() const {
     return m_garantie_luni;
 }
 
+// Setter cu validare pentru actualizarea garantiei
 void ProdusElectronic::SetGarantie(int luni) {
     _ValidareGarantie(luni);
     m_garantie_luni = luni;
 }
 
+// Afiseaza detalii despre produsul electronic (supraincarcare)
 void ProdusElectronic::AfiseazaDetalii() const {
     std::cout << "Produs Electronic: " << m_nume
               << " | Categorie: " << m_categorie
@@ -37,8 +44,10 @@ void ProdusElectronic::AfiseazaDetalii() const {
               << " | Stoc: " << m_stoc << "\n";
 }
 
+// Returneaza descriere detaliata a produsului (polimorfism)
 std::string ProdusElectronic::GetDescriereDetaliata() const {
     return "Electronic: " + m_nume + " cu garantie de " + std::to_string(m_garantie_luni) + " luni.";
 }
 
 } // namespace Online_Store
+
