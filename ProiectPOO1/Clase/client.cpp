86d8a34caf43be25f7435b57c1d782a2ae9f99cf
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 00:52:33 +0300

    Update client.cpp

--- ProiectPOO1/Clase/client.cpp
+++ ProiectPOO1/Clase/client.cpp
@@ -1,22 +1,29 @@
-#include "client.h"
 #include <iostream>
+#include "client.h"
 
 namespace Online_Store {
 
+/* Constructor implicit pentru Client.
+   Apeleaza constructorul implicit din Utilizator si seteaza adresa de livrare ca "necunoscuta". */
 Client::Client()
     : Utilizator(), m_adresa_livrare("necunoscuta") {}
 
+/* Constructor cu parametri pentru Client.
+   Initializeaza campurile mostenite si adresa de livrare. */
 Client::Client(int id, const std::string& nume_utilizator, const std::string& email, const std::string& adresa)
     : Utilizator(id, nume_utilizator, email), m_adresa_livrare(adresa) {}
 
+/* Adauga ID-ul unei comenzi in lista comenzilor clientului. */
 void Client::AdaugaComanda(int id_comanda) {
     m_id_comenzi.push_back(id_comanda);
 }
 
+/* Returneaza tipul utilizatorului - in acest caz, "Client". */
 std::string Client::GetTipUtilizator() const {
     return "Client";
 }
 
+/* Afiseaza profilul clientului: ID, nume, email, adresa de livrare si numarul de comenzi efectuate. */
 void Client::AfiseazaProfil() const {
     std::cout << "Profil Client:\n";
     std::cout << "ID: " << m_id << "\nNume: " << m_nume_utilizator
@@ -26,3 +33,4 @@ void Client::AfiseazaProfil() const {
 }
 
 } // namespace Online_Store
+
