562cc57b5a32c3bbcb6d6ce230c0624748f78c5a
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 11:37:56 +0300

    Update cos_cumparaturi.cpp

--- ProiectPOO1/Clase/cos_cumparaturi.cpp
+++ ProiectPOO1/Clase/cos_cumparaturi.cpp
@@ -6,8 +6,7 @@ namespace Online_Store {
 // Constructor implicit - initializeaza totalul pe 0
 CosCumparaturi::CosCumparaturi() : m_total(0.0) {}
 
-/* Functie privata care recalculeaza totalul cosului
-   pe baza preturilor produselor curente. */
+// Functie privata care recalculeaza totalul cosului pe baza preturilor produselor curente. 
 void CosCumparaturi::_ActualizeazaTotal() {
     m_total = 0.0;
     for (const auto& p : m_produse) {
@@ -15,14 +14,13 @@ void CosCumparaturi::_ActualizeazaTotal() {
     }
 }
 
-/* Adauga un produs in cos si actualizeaza totalul. */
+// Adauga un produs in cos si actualizeaza totalul.
 void CosCumparaturi::AdaugaProdus(const std::shared_ptr<Produs>& produs) {
     m_produse.push_back(produs);
     _ActualizeazaTotal();
 }
 
-/* Sterge produsul cu ID-ul specificat din cos, daca exista,
-   si actualizeaza totalul. */
+// Sterge produsul cu ID-ul specificat din cos, daca exista,si actualizeaza totalul. 
 void CosCumparaturi::StergeProdus(int id_produs) {
     m_produse.erase(
         std::remove_if(m_produse.begin(), m_produse.end(),
@@ -33,14 +31,13 @@ void CosCumparaturi::StergeProdus(int id_produs) {
     _ActualizeazaTotal();
 }
 
-/* Goleste complet cosul de cumparaturi si reseteaza totalul. */
+// Goleste complet cosul de cumparaturi si reseteaza totalul. 
 void CosCumparaturi::GolesteCosul() {
     m_produse.clear();
     m_total = 0.0;
 }
 
-/* Afiseaza toate produsele din cos si totalul curent.
-   Daca nu exista produse, afiseaza mesaj corespunzator. */
+// Afiseaza toate produsele din cos si totalul curent.Daca nu exista produse, afiseaza mesaj corespunzator. 
 void CosCumparaturi::AfiseazaCos() const {
     if (m_produse.empty()) {
         std::cout << "Cosul este gol.\n";
