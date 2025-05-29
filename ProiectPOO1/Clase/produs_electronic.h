6aac9d129351c755fa39cb5f7abf1369a2b12ff1
coondrea <99602467+coondrea@users.noreply.github.com>
2025-05-29 11:46:16 +0300

    Update produs_electronic.h

--- ProiectPOO1/Clase/produs_electronic.h
+++ ProiectPOO1/Clase/produs_electronic.h
@@ -10,18 +10,14 @@ class ProdusElectronic : public Produs {
 private:
     int m_garantie_luni; // numarul de luni garantie
 
-    // Metoda privata pentru validarea valorii garantiei (intre 0 si 60 luni)
+    
     void _ValidareGarantie(int luni);
 
 public:
-    // Constructor implicit
+    
     ProdusElectronic();
-
-    // Constructor cu parametri pentru initializarea completa a produsului
     ProdusElectronic(int id, const std::string& nume, const std::string& categorie,
                      double pret, int stoc, int garantie);
-
-    // Destructor
     ~ProdusElectronic();
 
     // Returneaza numarul de luni garantie
